fddf64062292fbc03854da97ff91b170f1b033c0
danielm-data <dmartin@stemdo.io>
2025-05-13 14:27:48 +0200

    Cambios

--- main.py
+++ main.py
@@ -7,6 +7,41 @@ st.title("Análisis de Clientes de Olist")
 df_customers = pd.read_csv('Olist_Data/olist_customers_dataset.csv')
 df_orders = pd.read_csv('Olist_Data/olist_orders_dataset.csv')
 
+string_columns_orders = [
+    'customer_id'
+    'order_id',
+    'order_status',
+]
+
+string_columns_customers = [
+    'customer_id',
+    'customer_unique_id',
+    'customer_city',
+    'customer_state',
+]
+
+date_columns = [
+    'order_purchase_timestamp',
+    'order_approved_at',
+    'order_delivered_carrier_date',
+    'order_delivered_customer_date',
+    'order_estimated_delivery_date',
+]
+# Cambiar el tipo de datos de las columnas de fecha a datetime
+df_orders[date_columns] = df_orders[date_columns].apply(pd.to_datetime)
+# Cambiar el tipo de datos de las columnas a striing
+df_orders[string_columns_orders] = df_orders[string_columns_orders].astype(str)
+df_customers[string_columns_customers] = df_customers[string_columns_customers].astype(str)
+
+# Si falta el valor de order_approved_at, se asume que el pedido fue aprobado en el momento 
+# de la compra
+df_orders["order_approved_at"].fillna(df_orders["order_purchase_timestamp"], inplace=True)
+# Si falta el valor de order_delivered_carrier_date, se asume que el pedido fue enviado 
+# un día después de la aprobación
+df_orders['order_delivered_carrier_date'].fillna(
+    df_orders['order_approved_at'] + pd.Timedelta(1), inplace=True
+)
+
 merged_df = pd.merge(df_orders, df_customers, on='customer_id', how='inner')
 
 pedidos_por_ciudad = (
